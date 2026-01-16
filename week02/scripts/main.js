5d2690df5bf922fc106f8c21a020e7bfe385e67e
Erlyn David Ascarate Lachi <erlyndavid@outlook.es>
2026-01-16 00:28:16 +0000

    Added the BOM Chapter adding

---
+++ week02/scripts/main.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('#favchap')
+const button = document.querySelector('button')
+const list = document.querySelector('#list ')
+
+button.addEventListener('click', () => {
+    if (input.value.trim() === '') {
+        alert('The input cannot be empty')
+    } else {
+        const li = document.createElement('li')
+        const deleteButton = document.createElement('button')
+
+        li.textContent = input.value
+        deleteButton.textContent = '❌'
+        deleteButton.ariaLabel = 'Close'
+
+        li.appendChild(deleteButton)
+        list.appendChild(li)
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li)
+            input.focus()
+        })
+    }
+
+    input.value = ''
+    input.focus()
+})
